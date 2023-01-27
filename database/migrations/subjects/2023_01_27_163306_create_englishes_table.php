57045872564f267c01b1ff4b5e8cae53d1bc4336
codejutsu1 <codejutsu@protonmail.com>
2023-01-27 18:00:19 +0100

    created individual table for each subjects with records of their scores

---
+++ database/migrations/subjects/2023_01_27_163306_create_englishes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('englishes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->string('class');
+            $table->string('first_ca');
+            $table->string('second_ca');
+            $table->string('exam');
+            $table->string('session');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('englishes');
+    }
+};
