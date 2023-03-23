8c04dc92457e7f911ca82911da6fac241f9f6298
codejutsu1 <codejutsu@protonmail.com>
2023-03-23 23:04:13 +0100

    record form class when permissison is granted and vice versa

--- app/Http/Livewire/ToggleButton.php
+++ app/Http/Livewire/ToggleButton.php
@@ -3,7 +3,9 @@
 namespace App\Http\Livewire;
 
 use App\Models\User;
+use App\Models\Teacher;
 use Livewire\Component;
+use App\Models\SubjectClass;
 use Illuminate\Database\Eloquent\Model;
 
 class ToggleButton extends Component
@@ -12,11 +14,15 @@ class ToggleButton extends Component
     public int $user;
     public string $field;
     public $individual;
+    public $classes;
 
     public function mount()
     {
         $this->individual = User::where('id', $this->user)->first();
+
         $this->isActive = (bool) $this->individual->hasPermissionTo($this->field);
+
+        $this->classes = SubjectClass::pluck('name')->toArray();
     }
 
     public function render()
@@ -26,6 +32,26 @@ class ToggleButton extends Component
 
     public function updating($field, $value)
     {
-        if($value) $this->individual->givePermissionTo($this->field); else $this->individual->revokePermissionTo($this->field);
+        if($value)
+        {
+            $this->individual->givePermissionTo($this->field);
+
+            if(in_array($this->field, $this->classes)){
+                Teacher::where('user_id', $this->user)->update([
+                    'formClass' => $this->field,
+                ]);
+            }
+
+        } 
+        else 
+        {
+            $this->individual->revokePermissionTo($this->field);
+
+            if(in_array($this->field, $this->classes)){
+                Teacher::where('user_id', $this->user)->update([
+                    'formClass' => NULL,
+                ]);
+            }
+        }
     }
 }
